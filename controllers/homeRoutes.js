78305a67df419f04431e1587e5f8b83880faee5f
hmustain <hunter@bunkerbranding.com>
2023-01-24 09:01:11 -0600

    working on first get route in the homeroutes folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const { Router } = require('express');
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+// setup GET route for the root URL / to get all post to display on the homepage
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            attributes: { exclude: ['password', 'email']},
+            order: [
+                ['date_created', 'DESC']
+            ]
+        });
+        
+        const posts = postData.map((post) => post.get({ plain: true}));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in,
+            });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
