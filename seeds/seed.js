417987ce8706ba0dc47465b19d121d7b3ef4a0b4
hmustain <hunter@bunkerbranding.com>
2023-01-24 13:26:57 -0600

    fixed a bug in my seed.js file where .json was not included in my requirements

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,9 +2,9 @@
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
-const userData = require('../seeds/userData');
-const postData = require('../seeds/postData');
-const commentData = require('../seeds/commentData');
+const userData = require('../seeds/userData.json');
+const postData = require('../seeds/postData.json');
+const commentData = require('../seeds/commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
