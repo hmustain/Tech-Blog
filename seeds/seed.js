8dffa4abab6e17b643833c7ac043a82ae6395ca1
hmustain <hunter@bunkerbranding.com>
2023-01-24 14:42:37 -0600

    updating my seed.js file to match act 23

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,9 +2,9 @@
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
-const userData = require('./userData');
-const postData = require('./postData');
-const commentData = require('./commentData');
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
