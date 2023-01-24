bca5fbcf8b349133a4d3bb82c2d8729ea579cae6
hmustain <hunter@bunkerbranding.com>
2023-01-24 10:59:26 -0600

    deleting unnecessary code homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { Router } = require('express');
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
