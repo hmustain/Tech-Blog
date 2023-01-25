5a48f2f93c1c445823be0465a15e2594aa6adbd8
hmustain <hunter@bunkerbranding.com>
2023-01-25 08:42:43 -0600

    working on api/postRoutes using MP for guidance

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,19 +3,19 @@ const { User, Post, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 // setup GET route for the root URL / to get all post to display on the homepage
-router.get("/", async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      order: [["date_created", "DESC"]],
-    });
-    console.log(postData);
-    const posts = postData.map((post) => post.get({ plain: true }));
+// router.get("/", async (req, res) => {
+//   try {
+//     const postData = await Post.findAll({
+//       order: [["date_created", "DESC"]],
+//     });
+//     console.log(postData);
+//     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.json(posts);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // router.get('/login', (req, res) => {
 //     if (req.session.logged_in) {
