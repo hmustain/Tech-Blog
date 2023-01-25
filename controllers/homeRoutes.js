995af5a89ae8e9ae19fdfab17247071c2aef0812
hmustain <hunter@bunkerbranding.com>
2023-01-25 13:00:30 -0600

    added handlebars for dashboard, homepage,login,post and updated routes for homeroute after tutor session

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,27 +3,72 @@ const { User, Post, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 // setup GET route for the root URL / to get all post to display on the homepage
-// router.get("/", async (req, res) => {
-//   try {
-//     const postData = await Post.findAll({
-//       order: [["date_created", "DESC"]],
-//     });
-//     console.log(postData);
-//     const posts = postData.map((post) => post.get({ plain: true }));
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      order: [["date_created", "DESC"]],
+      include: [{ model: User }]
+    });
+    console.log(postData);
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-//     res.json(posts);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('homepage', {posts});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/login', (req, res) => {
-//     if (req.session.logged_in) {
-//       res.redirect('/');
-//       return;
-//     }
+router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
 
-//     res.render('login');
-//   });
+      const post = postData.get({ plain: true });
+
+      res.render('post', {
+        ...post,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // Use withAuth middleware to prevent access to route
+router.get('/dashboard', async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      req.session.user_id=1  
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -6,4 +6,5 @@ module.exports = {
     format_amount: (amount) => {
       // format large numbers with commas
       return parseInt(amount).toLocaleString();
-    },
+    }
+  }
