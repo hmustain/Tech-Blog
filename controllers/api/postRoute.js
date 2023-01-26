18b88ce72c8c2e8ba0e4df2d3c944c9e8e254d39
hmustain <hunter@bunkerbranding.com>
2023-01-26 15:47:40 -0600

    updating api routes

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -16,6 +16,25 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    const postData = await Post.update({ 
+      title: req.body.title ,
+      content: req.body.content ,
+    },
+    {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id
+      }
+    })
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   console.log(req.session.user_id);
   try {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -64,6 +64,8 @@ router.get('/post/:id', async (req, res) => {
   
   });
 
+  
+
   // Use withAuth middleware to prevent access to route
 router.get('/dashboard', async (req, res) => {
     try {
@@ -88,7 +90,7 @@ router.get('/dashboard', async (req, res) => {
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/dashboard');
       return;
     }
   
