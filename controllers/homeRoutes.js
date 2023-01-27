33f7e7e72e52a399fe2ee3b9f38a2f976e180afa
hmustain <hunter@bunkerbranding.com>
2023-01-27 13:11:39 -0600

    resolved bug-acceptance-7

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
     console.log(postData);
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {posts});
+    res.render('homepage', {posts, logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
